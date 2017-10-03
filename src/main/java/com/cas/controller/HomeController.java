8348f3cd8da5796e90aa84fab75ae46f7267f68a
wangliucheng <liucheng.wang@mtime.com>
2017-10-04 00:20:16 +0800

    update

--- src/main/java/com/cas/controller/HomeController.java
+++ src/main/java/com/cas/controller/HomeController.java
@@ -1,17 +1,17 @@
 package com.cas.controller;
 
-import com.cas.model.RoleEnum;
+import com.cas.config.AuthorityInfo;
+import com.cas.model.UserInfo;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
+import java.util.Set;
 
-import static com.cas.model.RoleEnum.STUDENT;
 
 /**
  * Created by wangliucheng on 2017/9/19 0019.
@@ -19,29 +19,21 @@ import static com.cas.model.RoleEnum.STUDENT;
 @Controller
 public class HomeController {
 
+    //@PreAuthorize("hasAuthority('student')")//有TEST权限的才能访问
     @RequestMapping("/")
     public String index(Model model,HttpServletRequest request) {
 
         Principal userPrincipal = request.getUserPrincipal();
-        String name = userPrincipal.getName();
+        UserInfo userInfo = (UserInfo) SecurityContextHolder.getContext().getAuthentication() .getPrincipal();
+        String name = userInfo.getName();
+        String role = null;
+        Set<AuthorityInfo> authorities = userInfo.getAuthorities();
+        if(authorities.iterator().hasNext()){
+            AuthorityInfo next = authorities.iterator().next();
+            role = next.getAuthority();
+        }
+        request.setAttribute("role",role);
+        request.setAttribute("name",name);
         return "index";
-    }
-
-
-    @RequestMapping("/hello")
-    public String hello() {
-        return "不验证哦";
-    }
-
-    @PreAuthorize("hasAuthority('student')")//有TEST权限的才能访问
-    @RequestMapping("/security")
-    public String security() {
-        return "hello world security";
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")//必须要有ADMIN权限的才能访问
-    @RequestMapping("/authorize")
-    public String authorize() {
-        return "有权限访问";
     }
 }
